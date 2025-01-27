4d4e5e360b49682c2790f62896cdfa043d70bb82
Tri Ngoc <caotringoc11@gmail.com>
2025-01-27 23:03:26 +0700

    connect database, model, get task

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log('ok ');
+import express, { Express, Request, Response } from 'express';
+import * as database from './Config/database';
+import dotenv from 'dotenv';
+import Task from './Models/task.Model';
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: Number | String = process.env.PORT || 3001;
+
+app.get('/tasks', async (req: Request, res: Response) => {
+    const tasks = await Task.find({ deleted: false });
+    res.json({
+        code: 200,
+        message: 'list tasks',
+        tasks: tasks
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
